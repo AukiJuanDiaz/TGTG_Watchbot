c68d5fdb8d01f50837f2953c7bff4bd1faa4da29
Hauke <hauke.diers@outlook.com>
2021-03-26 01:11:43 +0100

    Using schedule librarary to recurrently check the API. Running locally

--- watch_script.py
+++ watch_script.py
@@ -1,6 +1,8 @@
 from tgtg import TgtgClient
 from json import load, dump
 import requests
+import schedule
+import time
 
 # Load tgtg account credentials from a hidden file
 f = open('telegram.json',)
@@ -41,7 +43,7 @@ def fetch_stock_from_api(api_result):
         current_fav['item_id'] = api_result[i]['item']['item_id']
         current_fav['store_name'] = api_result[i]['store']['store_name']
         current_fav['items_available'] = api_result[i]['items_available']
-        new_api_result.append(current_fav, ifmain)
+        new_api_result.append(current_fav)
 
     return new_api_result
 
@@ -49,6 +51,9 @@ def routine_check():
     """
     Function that gets called via schedule to get the api numbers and send a telegram message in case of a change
     """
+
+    # Get the global variable of items in stock
+    global favourites_in_stock
 
     # Get all favorite items
     api_response = client.get_items()
@@ -58,17 +63,34 @@ def routine_check():
     list_of_item_ids = [fav['item_id'] for fav in new_api_result]
     for item_id in list_of_item_ids:
         try:
-            old_stock = [item['items_available'] for item in favourites_in_stock if item['item_id'] == item_id]
+            old_stock = [item['items_available'] for item in favourites_in_stock if item['item_id'] == item_id][0]
         except:
             old_stock = 0
             print("An exception occurred: The item_id was not known as a favorite before")
 
-        new_stock = [item['items_available'] for item in new_api_result if item['item_id'] == item_id]
+        new_stock = [item['items_available'] for item in new_api_result if item['item_id'] == item_id][0]
 
         # Check, if the stock has changed. Send a message if so.
         if new_stock != old_stock:
             # Prepare a generic string, but with the important info
             message = f"There was a change in stock for the surprise bags at \
-                        {[item['store_name'] for item in new_api_result if item['item_id'] == item_id] }.\
+                        {[item['store_name'] for item in new_api_result if item['item_id'] == item_id][0] }.\
                         The old stock size was {old_stock}, the new stock size is {new_stock}."
             telegram_bot_sendtext(message)
+
+    # Reset the global information with the newest fetch
+    favourites_in_stock = new_api_result
+
+    # Print out some maintenance info in the terminal
+    print(f"API run at {time.ctime(time.time())} successful. Current stock:")
+    for item_id in list_of_item_ids:
+        print(f"{[item['store_name'] for item in new_api_result if item['item_id'] == item_id][0]}:\
+         {[item['items_available'] for item in new_api_result if item['item_id'] == item_id][0]}")
+
+# Use schedule to set up a recurrent checking
+schedule.every(1).minutes.do(routine_check)
+
+while True:
+    # run_pending
+    schedule.run_pending()
+    time.sleep(1)
