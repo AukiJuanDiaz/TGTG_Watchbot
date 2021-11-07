7758fb14b0dd2325f95af1475413a62495417e02
Kacper Huzar <kacper.huzar@forcom.com.pl>
2021-11-07 19:29:40 +0100

    fixed some bugs

--- watch_script.py
+++ watch_script.py
@@ -9,6 +9,7 @@ import json
 import maya
 import datetime
 import inspect
+from urllib.parse import quote
 
 try:
     filename = inspect.getframeinfo(inspect.currentframe()).filename
@@ -36,7 +37,7 @@ def telegram_bot_sendtext(bot_message, only_to_admin=True):
     It can be specified if both users or only the admin receives the message
     Follow this article to figure out a specific chatID: https://medium.com/@ManHay_Hong/how-to-create-a-telegram-bot-and-send-messages-with-python-4cf314d9fa3e
     """
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + str(config['telegram']["bot_chatID"]) + '&parse_mode=Markdown&text=' + bot_message
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + str(config['telegram']["bot_chatID"]) + '&parse_mode=Markdown&text=' + quote(bot_message)
     response = requests.get(send_text)
     return response.json()
 
@@ -48,7 +49,7 @@ def telegram_bot_sendimage(image_url, image_caption=None):
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendPhoto?chat_id=' + str(config['telegram']["bot_chatID"]) + '&photo=' + image_url
     # If the argument gets passed, at a caption to the image
     if image_caption != None:
-        send_text += '&parse_mode=Markdown&caption=' + image_caption
+        send_text += '&parse_mode=Markdown&caption=' + quote(image_caption)
     response = requests.get(send_text)
     return response.json()
 
@@ -56,7 +57,7 @@ def telegram_bot_delete_message(message_id):
     """
     For deleting a Telegram message
     """
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/deleteMessage?chat_id=' + str(config['telegram']["bot_chatID"]) + '&message_id=' + message_id
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/deleteMessage?chat_id=' + str(config['telegram']["bot_chatID"]) + '&message_id=' + str(message_id)
     response = requests.get(send_text)
     return response.json()
 
@@ -112,6 +113,10 @@ def toogoodtogo():
             old_stock = [stock['items_available'] for stock in tgtg_in_stock if stock['id'] == item['id']][0]
         except IndexError:
             old_stock = 0
+        try:
+            item['msg_id'] = [stock['msg_id'] for stock in tgtg_in_stock if stock['id'] == item['id']][0]
+        except:
+            pass
 
         new_stock = item['items_available']
 
@@ -136,8 +141,8 @@ def toogoodtogo():
                 # message = f" 📉 Decrease from {old_stock} to {new_stock} available goodie bags at {[item['store_name'] for item in new_api_result if item['item_id'] == item_id][0]}."
                 # telegram_bot_sendtext(message)
             elif old_stock > new_stock and new_stock == 0:
-                # message = f" ⭕ Sold out! There are no more goodie bags available at {item['store_name']}."
-                # telegram_bot_sendtext(message)
+                message = f" ⭕ Sold out! There are no more goodie bags available at {item['store_name']}."
+                telegram_bot_sendtext(message)
                 telegram_bot_delete_message([stock['msg_id'] for stock in tgtg_in_stock if stock['id'] == item['id']][0])
             else:
                 # Prepare a generic string, but with the important info
@@ -150,7 +155,7 @@ def toogoodtogo():
     # Print out some maintenance info in the terminal
     print(f"TGTG: API run at {time.ctime(time.time())} successful. Current stock:")
     for item in parsed_api:
-        print(f"{item['store_name']}: {item['items_available']}")
+        print(f"{item['store_name']}({item['id']}): {item['items_available']}")
 
 def parse_foodsi_api(api_result):
     """
@@ -173,30 +178,18 @@ def foodsi():
     Retrieves the data from foodsi API and selects the message to send.
     """
     req_json = """{
-        "page": 1,
-        "per_page": 50,
         "query": {
             "distance": {
                 "lat": "%s",
                 "lng": "%s",
                 "range": %s
-            },
-            "name": "",
-            "open": {
-                "from": "00:00",
-                "until": "24:00"
-            },
-            "sold_out": 0,
-            "types": [
-                "vegan",
-                "bakery",
-                "restaurant",
-                "shop"
-            ]
+            }
         },
         "user_favourites": 0
     }""" % (config['location']['lat'], config['location']['long'], config['location']['range']*1000)
     foodsi_api = requests.post('https://api.foodsi.pl/api/v1/restaurants', headers = {'Content-type':'application/json', 'system-version':'android_2.40.681', 'user-agent':'okhttp/3.12.0'}, data = req_json)
+    # print(json.dumps(json.loads(foodsi_api.text), sort_keys=True, indent=4))
+    # return
     items = parse_foodsi_api(foodsi_api.json())
     # Get the global variable of items in stock
     global foodsi_in_stock
@@ -207,6 +200,10 @@ def foodsi():
             old_stock = [stock['meals_left'] for stock in foodsi_in_stock if stock['id'] == item['id']][0]
         except IndexError:
             old_stock = 0
+        try:
+            item['msg_id'] = [stock['msg_id'] for stock in foodsi_in_stock if stock['id'] == item['id']][0]
+        except:
+            pass
 
         new_stock = item['meals_left']
 
@@ -220,6 +217,7 @@ def foodsi():
                 f"💰 *{item['meal']['price']}PLN*/{item['meal']['original_price']}PLN\n"\
                 f"⏰ {item['opened_at']}-{item['closed_at']}\n"\
                 "ℹ️ foodsi.pl"
+                # message += f"\ndebug id: {item['id']}"
                 tg = telegram_bot_sendimage(item['image'], message)
                 item['msg_id'] = tg['result']['message_id']
             elif old_stock > new_stock and new_stock != 0:
@@ -229,9 +227,9 @@ def foodsi():
                 # message = f" 📉 Decrease from {old_stock} to {new_stock} available goodie bags at {[item['store_name'] for item in new_api_result if item['id'] == item_id][0]}."
                 # telegram_bot_sendtext(message)
             elif old_stock > new_stock and new_stock == 0:
-                # message = f" ⭕ Sold out! There are no more goodie bags available at {item['store_name']}."
-                # telegram_bot_sendtext(message)
-                telegram_bot_delete_message([stock['msg_id'] for stock in tgtg_in_stock if stock['id'] == item['id']][0])
+                message = f" ⭕ Sold out! There are no more goodie bags available at {item['store_name']}."
+                telegram_bot_sendtext(message)
+                telegram_bot_delete_message([stock['msg_id'] for stock in foodsi_in_stock if stock['id'] == item['id']][0])
             else:
                 # Prepare a generic string, but with the important info
                 message = f"There was a change of number of goodie bags in stock from {old_stock} to {new_stock} at {item['store_name']}."
@@ -242,8 +240,8 @@ def foodsi():
 
     # Print out some maintenance info in the terminal
     print(f"Foodsi: API run at {time.ctime(time.time())} successful. Current stock:")
-    for item in items:
-        print(f"{item['store_name']}: {item['meals_left']}")
+    for item in foodsi_in_stock:
+        print(f"{item['store_name']}({item['id']}): {item['meals_left']}")
 
 
 def still_alive():
@@ -270,7 +268,7 @@ schedule.every(1).minutes.do(refresh)
 schedule.every(24).hours.do(still_alive)
 
 # Description of the sercive, that gets send once
-telegram_bot_sendtext("The bot script has started successfully. The bot checks every 1 minutes, if there is something new at TooGoodToGo or Foodsi. Every 24 hours, the bots sends a \"still alive\" message.")
+telegram_bot_sendtext("The bot script has started successfully. The bot checks every 1 minute, if there is something new at TooGoodToGo or Foodsi. Every 24 hours, the bots sends a \"still alive\" message.")
 refresh()
 while True:
     # run_pending
