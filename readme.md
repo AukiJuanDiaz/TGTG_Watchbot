caf3bca94faa38ef936153f6fe38a62afbcfd521
Kacper Huzar <kacper.huzar@forcom.com.pl>
2022-09-25 17:51:46 +0200

    Moved to foodsi v2 api, Fixed tgtg auth, Bug fixes

--- readme.md
+++ readme.md
@@ -12,12 +12,22 @@ This application scrapes info from the Too good to go and Foodsi and sends me a 
 Here is a screenshot:
 ![Telegram Screenshot](/result_screenshot.jpeg "Telegram bot with notifications")
 
-
 #### Tgtg API
 There is a library wrapped around the API of the tgtg-app. You can find the library and a short documentation [here.](https://pypi.org/project/tgtg/)
-##### How do I obtain credentails for tgtg?
-Just run: 
-```python3 watch_script.py <your_email>```
 
 #### Telegram bot
 I used Telegram as the service to notify me, because they are quite supportive for adding your own bots to the platform and provide a rich API. [This article](https://medium.com/@ManHay_Hong/how-to-create-a-telegram-bot-and-send-messages-with-python-4cf314d9fa3e) provides a quick introduction into sending Telegram messages with python.
+
+### Usage:
+#### Install required libraries:
+```pip install -r requirements.txt```
+#### Create config file:
+```cp config.example.json config.json```
+#### Edit config file:
+- Insert your bot token (you can get it from [@BotFather](https://t.me/BotFather))
+- Insert your location info (i use [latlong.net](https://www.latlong.net/))
+- Insert notification range (in kilometers)
+#### Run the script:
+```python3  watch_script.py```
+
+ On the first run the script will ask for your tgtg email address to get the needed API keys, and it will ask you to authorize your Telegram account by sending 6 digit pin to the bot.
--- watch_script.py
+++ watch_script.py
@@ -11,6 +11,9 @@ import datetime
 import inspect
 import sys
 from urllib.parse import quote
+import random
+import string
+import dateutil.parser
 
 try:
     filename = inspect.getframeinfo(inspect.currentframe()).filename
@@ -18,33 +21,76 @@ try:
     # Load credentials from a file
     f = open(os.path.join(path, 'config.json'), mode='r+')
     config = load(f)
-    if len(sys.argv) == 2:
-        try:
-            client = TgtgClient(email=sys.argv[1])
-            tgtg_creds = client.get_credentials()
-            print(tgtg_creds)
-            config['tgtg'] = tgtg_creds
-            f.seek(0)
-            json.dump(config, f, indent = 4)
-            f.truncate()
-        except:
-            print(traceback.format_exc())
-            exit(1)
-    f.close()
-except:
+except FileNotFoundError:
     print("No files found for local credentials.")
+    exit(1)
+except:
+    print("Unexpected error")
+    print(traceback.format_exc())
     exit(1)
 
 try:
     # Create the tgtg client with my credentials
-    tgtg_client = TgtgClient(access_token=config['tgtg']['access_token'], refresh_token=config['tgtg']['access_token'], user_id=config['tgtg']['access_token'])
+    tgtg_client = TgtgClient(access_token=config['tgtg']['access_token'], refresh_token=config['tgtg']['refresh_token'], user_id=config['tgtg']['user_id'])
+except KeyError:
+    # print(f"Failed to obtain TGTG credentials.\nRun \"python3 {sys.argv[0]} <your_email>\" to generate TGTG credentials.")
+    # exit(1)
+    try:
+        email = input("Type your TooGoodToGo email address: ")
+        client = TgtgClient(email=email)
+        tgtg_creds = client.get_credentials()
+        print(tgtg_creds)
+        config['tgtg'] = tgtg_creds
+        f.seek(0)
+        json.dump(config, f, indent = 4)
+        f.truncate()
+        tgtg_client = TgtgClient(access_token=config['tgtg']['access_token'], refresh_token=config['tgtg']['refresh_token'], user_id=config['tgtg']['user_id'])
+    except:
+        print(traceback.format_exc())
+        exit(1)
 except:
-    print(f"Failed to obtain TGTG credentials.\nRun \"python3 {sys.argv[0]} <your_email>\" to generate TGTG credentials.")
+    print("Unexpected error")
+    print(traceback.format_exc())
     exit(1)
 try:
     bot_token = config['telegram']["bot_token"]
-except:
+    if bot_token == "BOTTOKEN":
+        raise KeyError
+except KeyError:
     print(f"Failed to obtain Telegram bot token.\n Put it into config.json.")
+    exit(1)
+except:
+    print(traceback.format_exc())
+    exit(1)
+
+try:
+    bot_chatID = str(config['telegram']["bot_chatID"])
+    if bot_chatID == "0":
+        # Get chat ID
+        pin = ''.join(random.choice(string.digits) for x in range(6))
+        print("Please type \"" + pin + "\" to the bot.")
+        while bot_chatID == "0":
+            response = requests.get('https://api.telegram.org/bot' + bot_token + '/getUpdates?limit=1&offset=-1') 
+            # print(response.json())
+            if (response.json()['result'][0]['message']['text'] == pin):
+                bot_chatID = str(response.json()['result'][0]['message']['chat']['id'])
+                print("Your chat id:" + bot_chatID)
+                config['telegram']['bot_chatID'] = int(bot_chatID)
+                f.seek(0)
+                json.dump(config, f, indent = 4)
+                f.truncate()
+            time.sleep(1)
+except KeyError:
+    print(f"Failed to obtain Telegram chat ID.")
+    exit(1)
+except:
+    print(traceback.format_exc())
+    exit(1)
+
+try:
+    f.close()
+except:
+    print(traceback.format_exc())
     exit(1)
 
 # Init the favourites in stock list as a global variable
@@ -58,7 +104,7 @@ def telegram_bot_sendtext(bot_message, only_to_admin=True):
     It can be specified if both users or only the admin receives the message
     Follow this article to figure out a specific chatID: https://medium.com/@ManHay_Hong/how-to-create-a-telegram-bot-and-send-messages-with-python-4cf314d9fa3e
     """
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + str(config['telegram']["bot_chatID"]) + '&parse_mode=Markdown&text=' + quote(bot_message)
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + quote(bot_message)
     response = requests.get(send_text)
     return response.json()
 
@@ -67,7 +113,7 @@ def telegram_bot_sendimage(image_url, image_caption=None):
     For sending an image in Telegram, that can also be accompanied by an image caption
     """
     # Prepare the url for an telegram API call to send a photo
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendPhoto?chat_id=' + str(config['telegram']["bot_chatID"]) + '&photo=' + image_url
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendPhoto?chat_id=' + bot_chatID + '&photo=' + image_url
     # If the argument gets passed, at a caption to the image
     if image_caption != None:
         send_text += '&parse_mode=Markdown&caption=' + quote(image_caption)
@@ -78,7 +124,7 @@ def telegram_bot_delete_message(message_id):
     """
     For deleting a Telegram message
     """
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/deleteMessage?chat_id=' + str(config['telegram']["bot_chatID"]) + '&message_id=' + str(message_id)
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/deleteMessage?chat_id=' + bot_chatID + '&message_id=' + str(message_id)
     response = requests.get(send_text)
     return response.json()
 
@@ -92,14 +138,19 @@ def parse_tgtg_api(api_result):
         current_item = dict()
         current_item['id'] = store['item']['item_id']
         current_item['store_name'] = store['store']['store_name']
-        current_item['description'] = store['item']['description']
         current_item['items_available'] = store['items_available']
+        if current_item['items_available'] == 0:
+            result.append(current_item)
+            continue
+        current_item['description'] = store['item']['description']
         current_item['category_picture'] = store['item']['cover_picture']['current_url']
         current_item['price_including_taxes'] = str(store['item']['price_including_taxes']['minor_units'])[:-(store['item']['price_including_taxes']['decimals'])] + "." + str(store['item']['price_including_taxes']['minor_units'])[-(store['item']['price_including_taxes']['decimals']):]+store['item']['price_including_taxes']['code']
         current_item['value_including_taxes'] = str(store['item']['value_including_taxes']['minor_units'])[:-(store['item']['value_including_taxes']['decimals'])] + "." + str(store['item']['value_including_taxes']['minor_units'])[-(store['item']['value_including_taxes']['decimals']):]+store['item']['value_including_taxes']['code']
         try:
-            current_item['pickup_start'] = maya.when(store['pickup_interval']['start']).slang_date().capitalize() + " " + datetime.datetime.strptime(store['pickup_interval']['start'],'%Y-%m-%dT%H:%M:%S%z').strftime('%H:%M')
-            current_item['pickup_end'] = maya.when(store['pickup_interval']['end']).slang_date().capitalize() + " " + datetime.datetime.strptime(store['pickup_interval']['end'],'%Y-%m-%dT%H:%M:%S%z').strftime('%H:%M')
+            localPickupStart = datetime.datetime.strptime(store['pickup_interval']['start'],'%Y-%m-%dT%H:%M:%S%z').replace(tzinfo=datetime.timezone.utc).astimezone(tz=None)
+            localPickupEnd = datetime.datetime.strptime(store['pickup_interval']['end'],'%Y-%m-%dT%H:%M:%S%z').replace(tzinfo=datetime.timezone.utc).astimezone(tz=None)
+            current_item['pickup_start'] = maya.parse(localPickupStart).slang_date().capitalize() + " " + localPickupStart.strftime('%H:%M')
+            current_item['pickup_end'] = maya.parse(localPickupEnd).slang_date().capitalize() + " " + localPickupEnd.strftime('%H:%M')
         except KeyError:
             current_item['pickup_start'] = None
             current_item['pickup_end'] = None
@@ -124,8 +175,9 @@ def toogoodtogo():
         latitude=config['location']['lat'],
         longitude=config['location']['long'],
         radius=config['location']['range'],
+        page_size=300
     )
-    
+
     parsed_api = parse_tgtg_api(api_response)
 
     # Go through all favourite items and compare the stock
@@ -150,11 +202,17 @@ def toogoodtogo():
                 f"💰 *{item['price_including_taxes']}*/{item['value_including_taxes']}\n"
                 if 'rating' in item:
                     message += f"⭐️ {item['rating']}/5\n"
-                if 'opened_at' and 'closed_at' in item:
-                    message += f"⏰ {item['opened_at']}-{item['closed_at']}\n"
+                if 'pickup_start' and 'pickup_end' in item:
+                    message += f"⏰ {item['pickup_start']} - {item['pickup_end']}\n"
                 message += "ℹ️ toogoodtogo.com"
                 tg = telegram_bot_sendimage(item['category_picture'], message)
-                item['msg_id'] = tg['result']['message_id']
+                try: 
+                    item['msg_id'] = tg['result']['message_id']
+                except:
+                    print(json.dumps(tg))
+                    print(item['image']['url'])
+                    print(message)
+                    print(traceback.format_exc())
             elif old_stock > new_stock and new_stock != 0:
                 # customer feedback: This message is not needed
                 pass
@@ -174,9 +232,9 @@ def toogoodtogo():
     tgtg_in_stock = parsed_api
 
     # Print out some maintenance info in the terminal
-    print(f"TGTG: API run at {time.ctime(time.time())} successful. Current stock:")
-    for item in parsed_api:
-        print(f"{item['store_name']}({item['id']}): {item['items_available']}")
+    print(f"TGTG: API run at {time.ctime(time.time())} successful.")
+    # for item in parsed_api:
+    #     print(f"{item['store_name']}({item['id']}): {item['items_available']}")
 
 def parse_foodsi_api(api_result):
     """
@@ -184,41 +242,53 @@ def parse_foodsi_api(api_result):
     """
     new_api_result = list()
     # Go through all favorites linked to the account,that are returned with the api
-    for restaurant in api_result['restaurants']:
-        for item in restaurant['schedule']['days']:
-            if item['active'] == True:
-                current_item = item
-                current_item['store_name'] = restaurant['name']
-                current_item['meal'] = restaurant['meal']
-                current_item['image'] = restaurant['image']['url']
-                new_api_result.append(current_item)
+    for restaurant in api_result['data']:
+        current_item = restaurant
+        current_item['opened_at'] = dateutil.parser.parse(restaurant['package_day']['collection_day']['opened_at']).strftime('%H:%M')
+        current_item['closed_at'] = dateutil.parser.parse(restaurant['package_day']['collection_day']['closed_at']).strftime('%H:%M')
+        if (restaurant['package_day']['meals_left'] is None):
+            current_item['package_day']['meals_left'] = 0
+        new_api_result.append(current_item)
 
     return new_api_result
 def foodsi():
     """
     Retrieves the data from foodsi API and selects the message to send.
     """
-    req_json = """{
-        "query": {
+    items = list()
+    page = 1
+    totalpages = 1
+    while page <= totalpages:
+        req_json = {
+            "page": page,
+            "per_page": 15,
             "distance": {
-                "lat": "%s",
-                "lng": "%s",
-                "range": %s
+                "lat": config['location']['lat'],
+                "lng": config['location']['long'],
+                "range": config['location']['range']*1000
+            },
+            "hide_unavailable": False,
+            "food_type": [],
+            "collection_time": {
+                "from": "00:00:00",
+                "to": "23:59:59"
             }
-        },
-        "user_favourites": 0
-    }""" % (config['location']['lat'], config['location']['long'], config['location']['range']*1000)
-    foodsi_api = requests.post('https://api.foodsi.pl/api/v1/restaurants', headers = {'Content-type':'application/json', 'system-version':'android_2.40.681', 'user-agent':'okhttp/3.12.0'}, data = req_json)
-    # print(json.dumps(json.loads(foodsi_api.text), sort_keys=True, indent=4))
-    # return
-    items = parse_foodsi_api(foodsi_api.json())
+        }
+        foodsi_api = requests.post('https://api.foodsi.pl/api/v2/restaurants', headers = {'Content-type':'application/json', 'system-version':'android_3.0.0', 'user-agent':'okhttp/3.12.0'}, data = json.dumps(req_json))
+        items += parse_foodsi_api(foodsi_api.json())
+        # print("Foodsi current page: " + str(foodsi_api.json()['current_page']))
+        # print("Foodsi total pages: " + str(foodsi_api.json()['total_pages']))
+        totalpages = foodsi_api.json()['total_pages']
+        # print("Foodsi page count: " + str(len(foodsi_api.json()['data'])))
+        page += 1
+    print("Foodsi total items: " + str(len(items)))
     # Get the global variable of items in stock
     global foodsi_in_stock
 
     # Go through all favourite items and compare the stock
     for item in items:
         try:
-            old_stock = [stock['meals_left'] for stock in foodsi_in_stock if stock['id'] == item['id']][0]
+            old_stock = [stock['package_day']['meals_left'] for stock in foodsi_in_stock if stock['id'] == item['id']][0]
         except IndexError:
             old_stock = 0
         try:
@@ -226,21 +296,27 @@ def foodsi():
         except:
             pass
 
-        new_stock = item['meals_left']
+        new_stock = item['package_day']['meals_left']
 
         # Check, if the stock has changed. Send a message if so.
         if new_stock != old_stock:
             # Check if the stock was replenished, send an encouraging image message
             if old_stock == 0 and new_stock > 0:
                 #TODO: tommorrow date
-                message = f"🍽 There are {new_stock} new goodie bags at {item['store_name']}\n"\
+                message = f"🍽 There are {new_stock} new goodie bags at {item['name']}\n"\
                 f"_{item['meal']['description']}_\n"\
                 f"💰 *{item['meal']['price']}PLN*/{item['meal']['original_price']}PLN\n"\
                 f"⏰ {item['opened_at']}-{item['closed_at']}\n"\
-                "ℹ️ foodsi.pl"
+                f"ℹ️ Offer id: {item['id']}\n"\
                 # message += f"\ndebug id: {item['id']}"
-                tg = telegram_bot_sendimage(item['image'], message)
-                item['msg_id'] = tg['result']['message_id']
+                tg = telegram_bot_sendimage(item['image']['url'], message)
+                try: 
+                    item['msg_id'] = tg['result']['message_id']
+                except:
+                    print(json.dumps(tg))
+                    print(item['image']['url'])
+                    print(message)
+                    print(traceback.format_exc())
             elif old_stock > new_stock and new_stock != 0:
                 # customer feedback: This message is not needed
                 pass
@@ -260,9 +336,9 @@ def foodsi():
     foodsi_in_stock = items
 
     # Print out some maintenance info in the terminal
-    print(f"Foodsi: API run at {time.ctime(time.time())} successful. Current stock:")
-    for item in foodsi_in_stock:
-        print(f"{item['store_name']}({item['id']}): {item['meals_left']}")
+    print(f"Foodsi: API run at {time.ctime(time.time())} successful.")
+    # for item in foodsi_in_stock:
+    #     print(f"{item['name']}({item['id']}): {item['package_day']['meals_left']}")
 
 
 def still_alive():
@@ -278,7 +354,7 @@ def refresh():
     Retrieves the data from services APIs and selects the messages to send.
     """
     try:
-        # toogoodtogo()
+        toogoodtogo()
         foodsi()
     except:
         print(traceback.format_exc())
@@ -288,7 +364,7 @@ def refresh():
 schedule.every(1).minutes.do(refresh)
 schedule.every(24).hours.do(still_alive)
 
-# Description of the sercive, that gets send once
+# Description of the service, that gets send once
 telegram_bot_sendtext("The bot script has started successfully. The bot checks every 1 minute, if there is something new at TooGoodToGo or Foodsi. Every 24 hours, the bots sends a \"still alive\" message.")
 refresh()
 while True:
